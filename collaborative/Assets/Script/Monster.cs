fddfb9d6d8d11fd8644646670101f68a60b796f2
yoonseo0o <muddy0618@naver.com>
2025-05-10 20:05:03 +0900

    navmesh test

--- collaborative/Assets/Script/Monster.cs
+++ collaborative/Assets/Script/Monster.cs
@@ -4,10 +4,16 @@ using UnityEngine.UIElements;
 
 public class Monster : MonoBehaviour
 {
+    [Header("property")]
+    private float hp;//?
+    private float damageAmount;
+    [SerializeField] private float speed;
     NavMeshAgent agent;
     [SerializeField] private Transform target; 
+
     void Awake()
     {
+        agent.speed = speed;
         agent = GetComponent<NavMeshAgent>();
         if (target == null)
             target = GameObject.Find("Player").transform;
@@ -15,7 +21,11 @@ public class Monster : MonoBehaviour
      
     void Update()
     {
-        if(agent.destination != target.transform.position)
+        moveToTarget();
+    }
+    private void moveToTarget()
+    {
+        if (agent.destination != target.transform.position)
         {
             agent.SetDestination(target.transform.position);
         }
@@ -23,5 +33,30 @@ public class Monster : MonoBehaviour
         {
             agent.SetDestination(transform.position);
         }
+    }
+    private void Attack(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            other.GetComponent<Player>().Attacked(damageAmount);
+        else if (other.CompareTag("keep"))
+        {
+            // 지키는거 tag 확인 후,
+            // 지키는 거 클래스의 공격당하는 함수 실행
+        }
+        else
+        {
+            Debug.Log($"{name}이 {other.name}과 닿았지만 tag({other.tag})를 알 수 없음");
+        }
+    }
+    private void Attacked(float damageAmount)
+    {
+        if (IsDead())
+            return;
+        hp -= damageAmount;
+
+    }
+    private bool IsDead()
+    {
+        return hp < 0;
     }
 }
--- collaborative/Assets/Script/Player.cs
+++ collaborative/Assets/Script/Player.cs
@@ -3,56 +3,109 @@ using UnityEditor.AI;
 using UnityEngine.InputSystem;
 using static UnityEditor.Timeline.TimelinePlaybackControls;
 using UnityEngine.Windows;
-using System.Collections;
+using System.Collections; 
 public class Player : MonoBehaviour
 {
+    private float hp;
     [SerializeField] float moveSpeed;
     [SerializeField] float rotateSpeed;
      
     private InputAction moveAction;
     private InputAction lookAction;
     private Coroutine co;
+    private Vector3 moveInput;
+    private float maxSightAngle =60f;
     private float sensitivity=10;
      
-    void Awake()
+    private void Awake()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         lookAction = InputSystem.actions.FindAction("Look");
         moveAction.performed += OnMovePerformed;
         moveAction.canceled += OnMoveCanceled;
         lookAction.performed += OnLookPerformed; 
-    } 
-    void OnLookPerformed(InputAction.CallbackContext context)
-    {
-        Vector2 mouseDelta = context.ReadValue<Vector2>();
-        float yaw = mouseDelta.x * rotateSpeed * Time.deltaTime;
-        transform.Rotate(0, yaw, 0);
-    }
-
-    void OnMovePerformed(InputAction.CallbackContext context)
-    { 
-        if (co != null) { StopCoroutine(co); }
-        
-        Vector2 input = context.ReadValue<Vector2>();
-        co=StartCoroutine(move(new Vector3(input.x, 0, input.y)));
-    }
-    void OnMoveCanceled(InputAction.CallbackContext context)
-    { 
-        StopCoroutine (co); 
-    }
-    IEnumerator move (Vector3 pos)
-    {
-        while(true)
-        {
-            Vector3 moveDir = transform.right * pos.x + transform.forward * pos.z;
-            transform.localPosition += moveDir.normalized * moveSpeed * Time.deltaTime;
-            yield return null;
-        }
     }
     private void OnDisable()
     {
         moveAction.performed -= OnMovePerformed;
         moveAction.canceled -= OnMoveCanceled;
     }
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"triggerEnter - {other.gameObject.name}");
+    }
+    #region move&look
+    private float NormalizeAngle(float angle)
+    {
+        angle = angle % 360f;
+        if (angle > 180f) angle -= 360f;
+        return angle;
+    }
+    private void OnLookPerformed(InputAction.CallbackContext context)
+    {
+        Vector2 mouseDelta = context.ReadValue<Vector2>(); 
+
+        // 플레이어 y축 회전
+        float yaw = mouseDelta.x * rotateSpeed * Time.deltaTime;
+        transform.Rotate(0, yaw,0);
+
+        // 카메라 x축 회전
+        float cameraX = -mouseDelta.y * rotateSpeed * Time.deltaTime;
+        float currentAngle = NormalizeAngle(Camera.main.transform.localEulerAngles.x);
+        float nextAngle = currentAngle + cameraX;
+
+        if (nextAngle > -maxSightAngle && nextAngle < maxSightAngle)
+        {
+            Camera.main.transform.Rotate(cameraX, 0, 0);
+        }
+    }
+
+    private void OnMovePerformed(InputAction.CallbackContext context)
+    { 
+        Vector2 input = context.ReadValue<Vector2>();
+        moveInput = new Vector3(input.x, 0, input.y);
+        if (co == null)
+        {
+            co = StartCoroutine(move());
+        }
+    }
+    private void OnMoveCanceled(InputAction.CallbackContext context)
+    { 
+        if(co!=null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+
+    }
+    private IEnumerator move ()
+    {
+        while (true)
+        {
+            Vector3 moveDir = transform.right * moveInput.x + transform.forward * moveInput.z;
+            transform.localPosition += moveDir.normalized * moveSpeed * Time.deltaTime;
+             
+            yield return null;
+        }
+    }
+    private void jump()
+    {
+
+    }
+    #endregion
+
+    private void Attack()
+    {
+
+    }
+    public void Attacked(float damageAmount)
+    {
+        if (IsDead())
+            { return; }
+        hp-=damageAmount;
+    }
+    private bool IsDead()
+    {
+        return hp <= 0;
+    }
 }
